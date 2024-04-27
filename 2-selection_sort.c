e2ccd78bd31119a876eb3fc43b3059b43309ca30
Mahmoud <mah2002moud@gmail.com>
2024-04-27 23:46:08 +0300

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * selection_sort - sorts an array of integers
+ *
+ * @array: array of integers
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	size_t itr_size;
+	int *itr;
+	int min;
+	int min_index;
+
+	itr = array;
+	itr_size = size;
+	for (j = 0; j < size; j++, itr++, itr_size--)
+	{
+		min = itr[0];
+		min_index = 0;
+		for (i = 0; i < itr_size; i++)
+		{
+			if (itr[i] < min)
+			{
+				min = itr[i];
+				min_index = i;
+			}
+		}
+
+		itr[min_index] = itr[0];
+		itr[0] = min;
+		print_array(array, size);
+	}
+}
