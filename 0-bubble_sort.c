3ee6955503815256bbad1aaf9c4c5210d5e31172
Mahmoud <mah2002moud@gmail.com>
2024-04-28 01:19:56 +0300

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,16 +10,19 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int tmp;
+	char is_swaped = 1;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1 || is_swaped; i++)
 	{
+		is_swaped = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
+				is_swaped = 1;
 				tmp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = tmp;
